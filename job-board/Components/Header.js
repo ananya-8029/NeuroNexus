d23a574d6816fe76fbdcfe3db0c12d2acc94a8dd
Ananya Gupta <anaya29003@gmail.com>
2024-02-20 18:15:44 +0530

    Header Component

---
+++ job-board/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div className='h-96 w-12 bg-slate-800'>
+      <img src="/assests/LOGO.png" className='w-34 h-24'/>
+    </div>
+    </>
+  )
+}
+
+export default Header
