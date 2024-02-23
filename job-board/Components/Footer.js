125124437c1c68011124715c0e545c84970bcd59
Ananya Gupta <anaya29003@gmail.com>
2024-02-23 11:48:58 +0530

    Footer Component

--- job-board/Components/Footer.js
+++ job-board/Components/Footer.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Italiana } from "@next/font/google";
+
+const italiana = Italiana({ subsets: ["latin"], weight: ["400"] });
 
 const Footer = () => {
   return (
     <>
-      <div className="h-64 w-full bg-[#4F6F52]"></div>
+      <div className="flex h-64 w-full bg-[#4F6F52]">
+        <div className="w-1/4 flex justify-center items-center border-r-2 border-[#405842]">
+          <div className="w-[80%] h-[80%] flex justify-center items-center">
+            <div className={italiana.className}>
+              <p className="text-7xl text-[#f1f0f0]">
+                Career <br /> Craft
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="w-3/4"></div>
+      </div>
     </>
   );
 };
