9730ceaae8e534e157aabbaa66d6ee14246c8851
Ananya Gupta <anaya29003@gmail.com>
2024-02-23 11:38:51 +0530

    Footer Component

--- job-board/src/app/page.js
+++ job-board/src/app/page.js
@@ -1,10 +1,12 @@
+import Footer from "../../Components/Footer";
 import Header from "../../Components/Header";
 import LandingPage from "../../Components/LandingPage";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <LandingPage/>
-    </div>
+      <Footer/>
+    </>
   );
 }
